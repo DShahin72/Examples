39b30d2f5ce2ab054386b691c6d486be2c06007e
Dmitry Shakhin <dshahin@yandex.ru>
2023-07-08 16:12:23 +0300

    Поиск максимума из массива

--- Example009_IntroArray/Program.cs
+++ Example009_IntroArray/Program.cs
@@ -9,7 +9,11 @@ int Max(int arg1, int arg2, int arg3)
 }
 //               0   1   2   3   4   5   6   7   8
 int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
-array[0] = 12;
 
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
 
-System.Console.WriteLine(array[0]);
+System.Console.WriteLine(result);
