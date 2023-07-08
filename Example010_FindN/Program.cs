fe5c9169afd5846bc94c849ef4383e90c555138d
Dmitry Shakhin <dshahin@yandex.ru>
2023-07-08 17:00:27 +0300

    Поиск заданного числа в массиве

---
+++ Example010_FindN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти заданное число в массиве
+
+int[] array = { 1, 12, 34, 656, 43, 86, 6, 57, 43, 76 };
+
+int n = array.Length;
+//System.Console.WriteLine(N);
+
+int find = 43;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine($"Число {find} найдено, номер элемента массива - {index}");
+        break;
+    }
+    index++;
+}
